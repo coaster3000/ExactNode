873cf846be071bae77831490ed51e88d1d5ed303
coaster3000 <ckrier.3000@gmail.com>
2012-12-25 15:19:58 -0500

    Added block place perms

--- src/main/java/com/github/coaster3000/exactnode/listeners/BlockListener.java
+++ src/main/java/com/github/coaster3000/exactnode/listeners/BlockListener.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.Plugin;
 
 import com.github.coaster3000.exactnode.ExactNodeListener;
@@ -23,9 +23,9 @@ public class BlockListener extends ExactNodeListener {
 
 	public BlockListener(Plugin plugin, ConfigurationSection config, PlayerInformer informer) {
 		super(plugin, config, informer);
-		protectIgnite = config.getBoolean("check.ignition", protectIgnite);
-		blockbreak = config.getBoolean("check.block.break", blockbreak);
-		blockplace = config.getBoolean("check.block.place", blockplace);
+		protectIgnite = config.getBoolean("checks.ignition", protectIgnite);
+		blockbreak = config.getBoolean("checks.block.break", blockbreak);
+		blockplace = config.getBoolean("checks.block.place", blockplace);
 	}
 
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
@@ -38,6 +38,23 @@ public class BlockListener extends ExactNodeListener {
 		
 		if (useWildNode) {
 			perms.add(ROOTNODE + "." + NODE + ".break." + WILDCARD);
+			perms.add(ROOTNODE + "." + NODE + "." + WILDCARD + getBlockPermission(event.getBlock()));
+			perms.add(ROOTNODE + "." + NODE + "." + WILDCARD);
+			perms.add(ROOTNODE + "." + WILDCARD);
+		}
+		if (permissionDenied(event.getPlayer(), perms.toArray(new String[0])))
+			event.setCancelled(true);
+	}
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onPlace(BlockPlaceEvent event) {
+		if (!blockplace)
+			return;
+		
+		List<String> perms = new ArrayList<String>();
+		perms.add(ROOTNODE + "." + NODE + ".place." + getBlockPermission(event.getBlock()));
+		
+		if (useWildNode) {
+			perms.add(ROOTNODE + "." + NODE + ".place." + WILDCARD);
 			perms.add(ROOTNODE + "." + NODE + "." + WILDCARD + getBlockPermission(event.getBlock()));
 			perms.add(ROOTNODE + "." + NODE + "." + WILDCARD);
 			perms.add(ROOTNODE + "." + WILDCARD);
